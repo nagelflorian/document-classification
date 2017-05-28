a6ddc92da45207459c6b6d5d8375d742d202f8a1
Florian Nagel <hello@floriannagel.net>
2017-05-29 00:45:03 +0200

    Add development rest api

---
+++ src/api.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import json
+import requests
+import os, sys
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from flask import Flask, request
+
+app = Flask("document-classification")
+
+@app.route('/')
+def home():
+    return 'Visit the /prediction route with a url query param to get predictions'
+
+@app.route('/prediction')
+def prediction():
+    url = request.args.get('url')
+
+    r = requests.get(url, stream=True) #, stream=True)
+    r.raw.decode_content = True # Content-Encoding
+    im = Image.open(r.raw) #NOTE: it requires pillow 2.8+
+
+    # Read in the image_data
+    image_data = np.array(im)[:, :, 0:3]  # Select RGB channels only.
+
+    # Loads label file, strips off carriage return
+    label_lines = [line.rstrip() for line
+                       in tf.gfile.GFile("../model/retrained_labels.txt")]
+
+    # Unpersists graph from file
+    with tf.gfile.FastGFile("../model/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+
+    output = {}
+
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            output[human_string] = "%.9f" % score
+
+    return json.dumps(output)
